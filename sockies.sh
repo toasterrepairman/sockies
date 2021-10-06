d2e83a42c3f38668c8e2c362461c884b23ed640e
toast <smol@toast.cyou>
2021-10-06 00:56:02 -0400

    added netdata

--- sockies.sh
+++ sockies.sh
@@ -4,8 +4,11 @@ sudo pacman -S openssh yay micro fish
 # Enable remote access
 sudo systemctl status sshd.service
 
-# setting fish to default shell
+# Setting fish to default shell
 sudo chsh -s /bin/fish
+
+# Installing netdata
+bash <(curl -Ss https://my-netdata.io/kickstart.sh) — dont-wait
 
 # Download podman dependencies
 yay -S podman cockpit-podman cockpit-machines cockpit-podman
