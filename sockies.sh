d8616d4a93873f928dac2d17f7c2e8089892f9b5
toast <smol@toast.cyou>
2021-10-06 01:19:46 -0400

    more docker/podman garbage

--- sockies.sh
+++ sockies.sh
@@ -1,8 +1,13 @@
+# [BASIC SETUP]
 # Install basic tools
-sudo pacman -S openssh yay micro fish 
+sudo pacman -S openssh yay micro fish ufw
 
-# Enable remote access
-sudo systemctl status sshd.service
+# Enable SSH
+sudo ufw allow ssh
+
+# Enable services
+sudo systemctl enable sshd.service
+sudo systemctl enable cockpit
 
 # Setting fish to default shell
 sudo chsh -s /bin/fish
@@ -10,12 +15,25 @@ sudo chsh -s /bin/fish
 # Installing netdata
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) — dont-wait
 
-# Download podman dependencies
-yay -S podman cockpit-podman cockpit-machines cockpit-podman
-
 # Set up directory structure 
 cd ~/Documents/
 mkdir Code Books Cloud
 cd Code/ 
 mkdir Rust Shell Python
 cd
+
+# [PODMAN SETUP]
+# Login to dockerhub
+podman login -u toasterrepairman
+
+# Download podman dependencies
+yay -S podman cockpit-podman cockpit-machines cockpit-podman
+
+# Install grafana
+podman pull grafana/grafana
+
+# Install prometheus
+podman pull prom/prometheus
+
+# Boot containers
+# podman run -d --name=grafana -p 3000:3000 grafana/grafana
